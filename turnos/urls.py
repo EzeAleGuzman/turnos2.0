26329f66b16a8e88b4f8b99fc51387dc4c77a9eb
Katrina <katidblanco@gmail.com>
2023-11-29 19:30:51 -0300

    Probando commit

--- turnos/urls.py
+++ turnos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ver_medicos, name ='medicos'),
+    path('medicos/', views.ver_medicos, name ='medicos'),
     path('crearturno/', views.crear_turno, name= 'crear_turno'),
-    path('turnos/', views.ver_turnos, name = 'ver_turnos')
+    path('', views.ver_turnos, name = 'ver_turnos')
 ]

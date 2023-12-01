b5057f70251233abbc8177e8aaa5b8e1f5038a1d
Sarabe89 <ezealeguzman@gmail.com>
2023-12-01 00:02:20 -0300

    ya me canse.....

--- turnos/forms.py
+++ turnos/forms.py
@@ -6,7 +6,7 @@ class TurnoForm(forms.ModelForm):  # Use forms.ModelForm instead of just forms
         model = Turno
         fields = ['nombre','apellido', 'dni', 'medico', 'fecha', 'hora']
         widgets = {
-        'fecha': forms.DateInput(attrs={'type': 'date'}),
+        # 'fecha': forms.DateInput(attrs={'type': 'date'}),
         'hora': forms.TimeInput(format='%H:%M', attrs={'type': 'time'}),
 
 

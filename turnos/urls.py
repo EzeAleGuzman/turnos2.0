695d0ecb5f75f4f2f126da25658f890ef21cdcec
Sarabe89 <ezealeguzman@gmail.com>
2023-11-30 23:25:44 -0300

    funciones editar y borrar

--- turnos/urls.py
+++ turnos/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('medicos/', views.ver_medicos, name ='medicos'),
-    path('crearturno/', views.crear_turno, name= 'crear_turno'),
-    path('', views.ver_turnos, name = 'ver_turnos')
+    # path('crearturno/', views.crear_turno, name= 'crear_turno'),
+    path('editar_turno/<int:turno_id>/', views.editar_turno, name='editar_turno'),
+    path('borrar_turno/<int:turno_id>/', views.borrar_turno, name='borrar_turno'),
+    path("salir/", views.salir, name="salir"),
+    path('', views.ver_turnos, name = 'ver_turnos'),
 ]
--- turnos/views.py
+++ turnos/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Medico, Turno
 from .forms import TurnoForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib import messages
+
+def salir(request):
+    logout(request)
+    return redirect('/')
+
 
 def ver_medicos(request):
     medicos = Medico.objects.all()
+   
     return render(request, 'medicos.html', {'medicos': medicos})
 
-def crear_turno(request):
+
+@login_required
+def ver_turnos(request):
+    turnos = Turno.objects.all().order_by('horario')
+    
     if request.method == 'POST':
         form = TurnoForm(request.POST)
         form.save()
@@ -14,10 +27,28 @@ def crear_turno(request):
 
     else:
         form = TurnoForm()
+    pertenece_admin = request.user.groups.filter(name='admin').exists()
+    pertenece_secretario = request.user.groups.filter(name='secretario').exists()    
+    return render(request, 'turnos.html', {'turnos': turnos, 'form': form,'pertenece_admin': pertenece_admin,
+        'pertenece_secretario': pertenece_secretario})
 
-    return render(request, 'crearturno.html', {'form': form})
 
+def editar_turno(request, turno_id):
+    turno = get_object_or_404(Turno, id=turno_id)
 
-def ver_turnos(request):
-    turnos = Turno.objects.all()
-    return render(request, 'turnos.html', {'turnos': turnos})
+    if request.method == 'POST':
+        form = TurnoForm(request.POST, instance=turno)
+        if form.is_valid():
+            form.save()
+            return redirect('ver_turnos')
+    else:
+        form = TurnoForm(instance=turno)
+
+    return render(request, 'editar_turno.html', {'form': form})
+
+@login_required
+def borrar_turno(request, turno_id):
+    turno = get_object_or_404(Turno, id=turno_id)
+    if request.method == 'POST':
+        turno.delete()
+    return redirect('ver_turnos')
